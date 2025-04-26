c3cf4d6d9cb9449a881b8a060ea11d39f3d1e85a
Ryomaz <space.devb@gmail.com>
2025-04-25 22:36:23 -0500

    Primer commit

--- 1-todo-hooks-apis/src/App.jsx
+++ 1-todo-hooks-apis/src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
+
+const mocksTasks = [
+  {
+    id: crypto.randomUUID(),
+    name: "Do Homework",
+    completed:false
+  }, {
+    id: crypto.randomUUID(),
+    name: "Run 10km",
+    completed:false
+  }, {
+    id: crypto.randomUUID(),
+    name: "",
+    completed:false
+  }
+]
 
 const App = () => {
+  const [taskName, setTaskName] = useState("")
+  const [tasks, setTaks] = useState([])
   return (
-    <div>App</div>
+    <div>Todo List</div>
   )
 }
 
