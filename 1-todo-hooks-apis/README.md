bd03abf8af286507fcb756cb2a326500493d6111
Ryomaz <space.devb@gmail.com>
2025-04-27 21:57:16 -0400

    Añadiendo la funcionalidad sin context ni reducer aun

--- 1-todo-hooks-apis/README.md
+++ 1-todo-hooks-apis/README.md
@@ -1,8 +1,11 @@
-# 1️⃣ **To-Do List Avanzado**  
+# 1️⃣ **To-Do List Avanzado**
+
 🔹 **Hooks:** `useState`, `useEffect`, `useReducer`, `useContext`  
 🔹 **APIs:** `localStorage` (para persistencia)  
-🔹 **Extras:** Filtros (completados, pendientes), drag & drop (`react-dnd`), temas (dark/light mode con `useContext`).  
+🔹 **Extras:** Filtros (completados, pendientes), drag & drop (`react-dnd`), temas (dark/light mode con `useContext`).
 
 ## Steps
 
-- Construir la interfaz
+-  Construir la interfaz ✔️
+-  Hacer la funcionalidad sin reducer ni context ✔️
+-  Separar en componentes la barra de añadir y las tareas independientes
--- 1-todo-hooks-apis/src/App.jsx
+++ 1-todo-hooks-apis/src/App.jsx
@@ -1,28 +1,72 @@
-import React from 'react'
-import { useState } from 'react'
+import { useState } from "react";
 
 const mocksTasks = [
-  {
-    id: crypto.randomUUID(),
-    name: "Do Homework",
-    completed:false
-  }, {
-    id: crypto.randomUUID(),
-    name: "Run 10km",
-    completed:true
-  }, {
-    id: crypto.randomUUID(),
-    name: "Buy coffee",
-    completed:false
-  }
-]
+   {
+      id: crypto.randomUUID(),
+      name: "Do Homework",
+      completed: false,
+   },
+   {
+      id: crypto.randomUUID(),
+      name: "Run 10km",
+      completed: true,
+   },
+   {
+      id: crypto.randomUUID(),
+      name: "Buy coffee",
+      completed: false,
+   },
+];
 
-const App = () => {
-  const [taskName, setTaskName] = useState("")
-  const [tasks, setTaks] = useState([])
-  return (
-    <div>Todo List</div>
-  )
+function App() {
+   const [taskName, setTaskName] = useState("");
+   const [tasks, setTasks] = useState(mocksTasks);
+
+   const handleAdd = (e) => {
+      e.preventDefault();
+      const newTask = {
+         id: crypto.randomUUID(),
+         name: taskName,
+         completed: false,
+      };
+      setTasks([...tasks, newTask]);
+   };
+   const handleDelete = (id) => {
+      const withoutTask = tasks.filter((task) => task.id !== id);
+      setTasks(withoutTask);
+   };
+   const handleComplete = () => {};
+
+   return (
+      <>
+         <h1>Nice Todo App</h1>
+         <form>
+            <input
+               type="text"
+               placeholder="Things to do..."
+               value={taskName}
+               onChange={(e) => setTaskName(e.target.value)}
+            />
+            <button onClick={handleAdd}>+</button>
+         </form>
+         {tasks && tasks.length
+            ? tasks.map((task) => {
+                 const { id, name, completed } = task;
+                 return (
+                    <article
+                       key={id}
+                       className={completed ? "completed" : null}
+                    >
+                       <h3>{name}</h3>
+                       <button onClick={() => handleDelete(id)}>Delete</button>
+                       <button onClick={() => handleComplete(id)}>
+                          Complete
+                       </button>
+                    </article>
+                 );
+              })
+            : null}
+      </>
+   );
 }
-
-export default App
+export default App;
