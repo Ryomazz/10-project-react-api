b9917f3f6a50504089426506a185ce1ee854f185
Ryomaz <space.devb@gmail.com>
2025-05-09 07:28:59 -0400

    Terminado la barra de progreso

--- 4-reproductor-audio/README.md
+++ 4-reproductor-audio/README.md
@@ -3,3 +3,8 @@
 🔹 **Hooks:** `useState`, `useEffect`, `useRef`  
 🔹 **APIs:** `Web Audio API` o `<audio>` de HTML5  
 🔹 **Extras:** Playlist, barra de progreso, volumen, efectos (usando `useRef` para manipular el DOM directamente).
+
+## Steps ✔️
+
+-  Crear un array con los nombres y direcciones de las canciones ✔️
+-  Crear la primera interfaz
--- 4-reproductor-audio/src/App.jsx
+++ 4-reproductor-audio/src/App.jsx
@@ -1,4 +1,58 @@
+import { useEffect, useRef, useState } from "react";
+import { audioFiles } from "./audioFiles";
+import "../src/assets/Cancion (1).mp3";
+
 function App() {
-   return <div>App</div>;
+   const [songIndex, setSongIndex] = useState(0);
+   const [isPlaying, setIsPlaying] = useState(false);
+   const [progress, setProgress] = useState(0);
+   const audioRef = useRef(null);
+
+   const handlePlayPause = () => {
+      if (isPlaying) {
+         setIsPlaying(false);
+         audioRef.current.pause();
+      } else {
+         setIsPlaying(true);
+         audioRef.current.play();
+      }
+   };
+
+   const handleProgress = (e) => {
+      const currentProgress =
+         (e.target.value / 100) * audioRef.current.duration;
+      audioRef.current.currentTime = currentProgress;
+   };
+
+   const handleTimeUpdate = () => {
+      const currentTime = audioRef.current.currentTime;
+      const duration = audioRef.current.duration;
+      setProgress((currentTime / duration) * 100);
+   };
+
+   return (
+      <div className="audio-player">
+         <audio
+            src={audioFiles[songIndex]?.url}
+            ref={audioRef}
+            onTimeUpdate={handleTimeUpdate}
+         />
+         <article className="player-info">
+            <h2>{audioFiles[songIndex]?.name}</h2>
+            <input
+               type="range"
+               value={progress}
+               min="0"
+               max="100"
+               onChange={handleProgress}
+            />
+         </article>
+         <article className="player-controls">
+            <button>⏪</button>
+            <button onClick={handlePlayPause}>⏯️</button>
+            <button>⏩</button>
+         </article>
+      </div>
+   );
 }
 export default App;
